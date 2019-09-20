71e98dc4e01c63072a86f0b6e0bebac596e72f88
Serena Bailey <baileysn@gmatc.matc.edu>
2019-09-20 13:21:32 -0500

    Added Database connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 const app = express();
+
+connectDatabase();
 
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
