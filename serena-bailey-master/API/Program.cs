4440541ef50a6c59687c70b4a6839ef61406f454
Serena Bailey <baileysn@gmatc.matc.edu>
2019-10-10 20:53:38 -0500

    Completed Activity 5

--- serena-bailey-master/API/Program.cs
+++ serena-bailey-master/API/Program.cs
@@ -14,7 +14,25 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedData(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred during migration.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
