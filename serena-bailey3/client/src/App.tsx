5e821ddd780afcef7bb8e2ce3af51bff733c784c
Serena Bailey <baileysn@gmatc.matc.edu>
2019-10-17 18:13:07 -0500

    Completed Activity 5

---
+++ serena-bailey3/client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          GoodThings
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
